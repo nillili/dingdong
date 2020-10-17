1a099f5d16ee1c13a22ee545685d5db37e96b765
nillili <ds1lph@gmail.com>
2020-10-17 16:08:01 +0900

    1차 마무리

--- dingdong/dingdong.ino
+++ dingdong/dingdong.ino
@@ -1,23 +1,33 @@
 #define BLYNK_PRINT Serial
 
-
+#include "userinfo.h"
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
+#include <TimeLib.h>
+#include <WidgetRTC.h>
 SimpleTimer timer;
+WidgetRTC rtc;
 
-
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
-char auth[] = "YJIRLSZpqVTjMKhw0e6jHiS0m_L_IPyF";
-
-// Your WiFi credentials.
-// Set password to "" for open networks.
-char ssid[] = "neozi3";
-char pass[] = "6259956";
+extern const char auth[];
+extern const char ssid[];
+extern const char pass[];
 
 
 int inputPin = 13;
 int howmany  = 500;
+int is_touch = 0;
+int scase = 1;//  상황 1:출근, 2:퇴근
+
+String currentDate = "";    
+String currentTime = "";
+
+// 지연시간 설정
+unsigned long previousMillis  = 0;
+unsigned long currentMillis   = 0;
+int interval=1000;
+int delay_time = -1;     // 지연시간(분)
+
+WidgetTerminal terminal(V3);
 
 
 BLYNK_WRITE(V1) //Button Widget is writing to pin V1
@@ -28,16 +38,40 @@ BLYNK_WRITE(V1) //Button Widget is writing to pin V1
   Serial.println(howmany);
 }
 
+BLYNK_WRITE(V5) 
+{
+  switch(param.asInt())
+  {
+    case 1:
+      Serial.println("case1");  
+      scase = 1;
+      break;
+    case 2:
+      Serial.println("case2");  
+      scase = 2;
+      break;      
+    default:
+      scase = 1;
+      
+  }
+}
+BLYNK_WRITE(V9) 
+{
+ terminal.clear();
+}
 
 void setup()
 {
   // Debug console
-//  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
   pinMode(inputPin, INPUT);
   
   Blynk.begin(auth, ssid, pass);
   timer.setInterval(500L, sendSensor);
 
+
+  rtc.begin();            // 위젯 시간
+  
   Serial.begin(9600);
 }
 
@@ -63,27 +97,45 @@ void loop()
 
 void sendSensor()
 {
-    // 내용
-Serial.println("------------------>1");  
-//  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
-//  // but actually the LED is on; this is because
-//  // it is active low on the ESP-01)
-//Serial.println("------------------>2");  
-//  delay(howmany);                      // Wait for a second
-//Serial.println("------------------>3");  
-//  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-//Serial.println("------------------>4");  
-//  delay(howmany);    
+  // 지연 시간동안 들어 가지 못한다
+  if((unsigned long)(millis() - previousMillis) >= interval)
+  {
+      // 내용
+    Serial.println("------------------>");  
+  
+    int val = digitalRead(inputPin);
+    Serial.println(val);
+    if(val == HIGH){
+      Serial.println("감지");    
+      is_touch = 1;
+      digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+      Blynk.virtualWrite(V2, 1);
 
-
-  int val = digitalRead(inputPin);
-  Serial.println(val);
-  if(val == HIGH){
-    Serial.println("감지");      
-    Blynk.virtualWrite(V2, 1);
-  }else{
-    Blynk.virtualWrite(V2, 0);    
+      currentDate = String(year()) + "/" + month() + "/"+ day() + " " ;    
+      currentTime = String(hour()) + ":" + minute() + ":" + second();    
+      
+      if(scase == 1)
+      {
+        Blynk.notify("alert - 딩동");
+      }else if(scase == 2)
+      {
+        Blynk.email("ds1lph@gmail.com",
+        "blynk 알림 - dingdong - "+currentDate + currentTime,
+        "접근 확인"+currentDate + currentTime);        
+      }
+      
+      terminal.println("Detect:" +currentDate + currentTime);
+      terminal.flush();      
+      snapshotTime(5000);  // 5초 얼음
+      
+    }else{
+      if(is_touch == 1)
+      {
+        is_touch = 0;
+        Blynk.virtualWrite(V2, 0);    
+        digitalWrite(LED_BUILTIN, LOW);  // Turn the LED off by making the voltage HIGH
+      }
+    }
   }
-
   
 }
